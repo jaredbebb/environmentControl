f5da40b4d81d2a75dbc2dc9558922be4f624ec99
Jared Bebb <j.bebb@live.com>
2020-01-19 10:54:40 -0600

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+  Serial.println("loop function");
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(5000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
